ea847704ef1514018710e5c007537d1a4a77d782
73nko <apramos89@gmail.com>
2024-08-18 17:27:06 +0200

    Continue with nvim configuration

--- nvim/lua/alex/core/keymaps.lua
+++ nvim/lua/alex/core/keymaps.lua
@@ -3,67 +3,67 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 
 keymap.set("i", "jk", "<ESC>", {
-    desc = "Exit insert mode with jk"
+  desc = "Exit insert mode with jk",
 })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", {
-    desc = "Clear search highlights"
+  desc = "Clear search highlights",
 })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", {
-    desc = "Increment number"
+  desc = "Increment number",
 }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", {
-    desc = "Decrement number"
+  desc = "Decrement number",
 }) -- decrement
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", {
-    desc = "Split window vertically"
+  desc = "Split window vertically",
 }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", {
-    desc = "Split window horizontally"
+  desc = "Split window horizontally",
 }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", {
-    desc = "Make splits equal size"
+  desc = "Make splits equal size",
 }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", {
-    desc = "Close current split"
+  desc = "Close current split",
 }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {
-    desc = "Open new tab"
+  desc = "Open new tab",
 }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {
-    desc = "Close current tab"
+  desc = "Close current tab",
 }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {
-    desc = "Go to next tab"
+  desc = "Go to next tab",
 }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {
-    desc = "Go to previous tab"
+  desc = "Go to previous tab",
 }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {
-    desc = "Open current buffer in new tab"
+  desc = "Open current buffer in new tab",
 }) --  move current buffer to new tab
 
-vim.api.nvim_set_keymap('n', '<leader>shd', ':sp | lua vim.lsp.buf.definition()<CR>', {
-    noremap = true,
-    silent = true
+keymap.set("n", "<leader>shd", ":sp | lua vim.lsp.buf.definition()<CR>", {
+  noremap = true,
+  silent = true,
 }) -- Abrir definición en split horizontal
 
-vim.api.nvim_set_keymap('n', '<leader>shi', ':sp | lua vim.lsp.buf.implementation()<CR>', {
-    noremap = true,
-    silent = true
+keymap.set("n", "<leader>shi", ":sp | lua vim.lsp.buf.implementation()<CR>", {
+  noremap = true,
+  silent = true,
 }) -- Abrir implementación en split horizontal
 
-vim.api.nvim_set_keymap('n', '<leader>svd', ':vsp | lua vim.lsp.buf.definition()<CR>', {
-    noremap = true,
-    silent = true
+keymap.set("n", "<leader>svd", ":vsp | lua vim.lsp.buf.definition()<CR>", {
+  noremap = true,
+  silent = true,
 }) -- Abrir definición en split vertical
 
-vim.api.nvim_set_keymap('n', '<leader>svi', ':vsp | lua vim.lsp.buf.implementation()<CR>', {
-    noremap = true,
-    silent = true
+keymap.set("n", "<leader>svi", ":vsp | lua vim.lsp.buf.implementation()<CR>", {
+  noremap = true,
+  silent = true,
 }) -- Abrir implementación en split vertical
