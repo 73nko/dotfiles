004c22067880ed0f6b3a7e7286430f2dfa3cafa6
73nko <apramos89@gmail.com>
2024-08-23 14:53:05 +0200

    Finally copilot activated

--- nvim/lua/alex/plugins/nvim-cmp.lua
+++ nvim/lua/alex/plugins/nvim-cmp.lua
@@ -45,6 +45,8 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
+        { name = "copilot" },
+        { name = "tabnine" },
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
